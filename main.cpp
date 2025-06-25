129c326278c0a86209406462e8c9c40321b4f502
SamSepi2 <72649220@continental.edu.pe>
2025-06-24 19:38:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+//funciones base del arbol 
+struct Persona {
+    string nombre;
+    int edad;
+    int id;
+    Persona* izq;
+    Persona* der;
+};
 
