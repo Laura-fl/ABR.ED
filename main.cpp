385a634914a5013254266a93abadf76112686f92
SamSepi2 <72649220@continental.edu.pe>
2025-06-24 19:39:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,4 +10,12 @@ struct Persona {
     Persona* izq;
     Persona* der;
 };
-
+Persona* nuevaPersona(string nombre, int edad, int id) {
+    Persona* nueva = new Persona();
+    nueva->nombre = nombre;
+    nueva->edad = edad;
+    nueva->id = id;
+    nueva->izq = NULL;
+    nueva->der = NULL;
+    return nueva;
+}
