cca4ca9c4b4b49802d166677a3e8e6d9e976e1f9
SamSepi2 <72649220@continental.edu.pe>
2025-06-24 19:41:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,4 +20,13 @@ Persona* nuevaPersona(string nombre, int edad, int id) {
     nueva->der = NULL;
     return nueva;
 }
+Persona* insertar(Persona* raiz, string nombre, int edad, int id) {
+    if (raiz == NULL)
+        return nuevaPersona(nombre, edad, id);
+    if (edad < raiz->edad)
+        raiz->izq = insertar(raiz->izq, nombre, edad, id);
+    else
+        raiz->der = insertar(raiz->der, nombre, edad, id);
+    return raiz;
+}
 
